bed5de8dbb8bea0b6af93b9e4800eb1d1a40f742
pavuluri sivakrishna <pavulurisivakrishna54@gmail.com>
2026-02-20 06:36:59 +0530

    ec2.tf

---
+++ ec2/ec2.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "example" {
+  ami           = "ami-0220d79f3f480ecf5"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "terraform-practice"
+    project = "roboshop"
+  }
+}
